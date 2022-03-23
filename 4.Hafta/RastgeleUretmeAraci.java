4c93a546da323698f51059d8c2377b63b98b2480
NurBanu <nbogur@sakarya.edu.tr>
2022-03-23 09:51:40 +0300

    Uygulama4

---
+++ 4.Hafta/RastgeleUretmeAraci.java
@@ -0,0 +1,14 @@
+package nb.ders5.lab;
+
+import java.util.Random;
+
+public class RastgeleUretmeAraci {
+
+    public static String rastgeleKelimeUret (int harfSayisi) {
+        Random r = new Random();
+        StringBuilder sb = new StringBuilder(harfSayisi);
+        for(int i = 0; i < harfSayisi; i++)
+            sb.append((char)('a' + (r.nextInt('z' - 'a'))));
+        return sb.toString();
+    }
+}
