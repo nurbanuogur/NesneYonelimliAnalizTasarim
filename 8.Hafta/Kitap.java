338fa3fa99b9fc7086035e0f55f05c1a13adcb16
NurBanu <nbogur@sakarya.edu.tr>
2022-04-25 16:54:37 +0300

    Uygulama8

---
+++ 8.Hafta/Kitap.java
@@ -0,0 +1,58 @@
+package nb.ders8;
+
+import java.util.ArrayList;
+
+public class Kitap {
+    private String baslik;
+    private int toplamSayfaSayisi;
+    private ArrayList<Yazar> yazarlar;
+    private ArrayList<Sayfa> sayfalar;
+
+    public Kitap(String baslik) {
+        this.baslik = baslik;
+        this.toplamSayfaSayisi = 0;
+        this.yazarlar =new ArrayList<Yazar>();
+        this.sayfalar=new ArrayList<Sayfa>();
+    }
+
+    public String getBaslik() {
+        return baslik;
+    }
+
+    public void setBaslik(String baslik) {
+        this.baslik = baslik;
+    }
+
+    public int getToplamSayfaSayisi() {
+        return toplamSayfaSayisi;
+    }
+
+    public void sayfaEkle(Sayfa sayfa){
+        sayfalar.add(sayfa);
+        toplamSayfaSayisi++;
+    }
+    public void yazarEkle(Yazar yazar){
+        yazarlar.add(yazar);
+    }
+    public Sayfa sayfayiGetir(int sayfaNo){
+        Sayfa sayfa= sayfalar
+                .stream()
+                .filter(item -> item.getSayfaNumarasi()==sayfaNo)
+                .findFirst()
+                .orElse(null);
+        return  sayfa;
+    }
+    public void tumSayfalariYazdir() {
+        sayfalar.forEach(item -> System.out.println(item));
+    }
+
+    @Override
+    public String toString() {
+        return "Kitap{" +
+                "baslik='" + baslik + '\'' +
+                ", toplamSayfaSayisi=" + toplamSayfaSayisi +
+                ", yazarlar=" + yazarlar +
+                ", sayfalar=" + sayfalar +
+                '}';
+    }
+}
---
+++ 8.Hafta/README.md
@@ -0,0 +1,19 @@
+
+## İstenenler:
+
+* Geçen hafta (7.Hafta) geliştirilen uygulamaya eklemeler yapmanız beklenmektedir. Mevcut sınıfları kullanınız.
+* KutuphanedekiKitap (Kitap sınıfından türetilmeli), Kütüphane ve Uygulama sınıflarını oluşturunuz.
+
+
+Sınıf şeması verilen uygulamayı geliştiriniz. “main” yöntemi içerisinde:
+
+* kutuphane nesnesi oluşturunuz.
+* ikişer sayfalı ve ikişer yazarlı iki kitap (KutuphanedekiKitap nesnesinden) oluşturunuz.
+* bu kitapları kütüphane nesnesine ekleyiniz.
+* kütüphanedeki tüm kitapların yazarlarını listeleyiniz.
+
+
+![](https://github.com/nurbanuogur/NesneYonelimliAnalizTasarim/blob/master/Diyagramlar/Uygulama8.png)
+
+
+ 	
